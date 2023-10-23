704b60024751f5107be419ab8596e4e75ac4dec4
AXUNDA <charlesemmanuelazunda@gmail.com>
2023-10-23 16:07:53 +0100

    fixes

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
-import Shield from "./components/Shield";
 
 const inter = Inter({ subsets: ["latin"] });
 import "react-pro-sidebar/dist/css/styles.css";
